20e4306530c7a5112ac03e06dd9d20b258b69358
Nancy Bui <n6bui@ucsd.edu>
2018-02-15 10:52:10 -0800

    lab 6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,6 +14,18 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
+function addProject(result) {
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>'; 
+}
+
+function callback(){
+	console.log(result);
+}
+
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
